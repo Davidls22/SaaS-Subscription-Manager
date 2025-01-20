d10f5ed8ff2fb8ac88ec1e1b0bce48560662f802
David Sugden <david-sugden1@hotmail.co.uk>
2025-01-20 21:33:23 +0000

    fix api

--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://saas-subscription-manager.onrender.com/",
+  baseURL: "https://saas-subscription-manager.onrender.com/api",
 });
 
 API.interceptors.request.use((config:any) => {
