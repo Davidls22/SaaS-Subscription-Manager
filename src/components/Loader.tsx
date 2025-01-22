4b32636f7028766ca048b6b546be2a2ad3740b81
David Sugden <david-sugden1@hotmail.co.uk>
2025-01-22 18:14:18 +0000

    amend loader styling

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,7 @@
 
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center">
       <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent border-solid rounded-full animate-spin"></div>
     </div>
   );
